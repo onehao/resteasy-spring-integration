a31c946fe5a3eb4b62329b5ed499969ab2ca35ef
onehao <onehao333@gmail.com>
2016-05-25 15:53:39 +0800

    : resteasy xml  support.
    Task-Url:

--- springrest/src/com/onehao/rest/HealthCheck.java
+++ springrest/src/com/onehao/rest/HealthCheck.java
@@ -2,9 +2,12 @@ package com.onehao.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import org.jboss.resteasy.annotations.providers.jackson.Formatted;
 
@@ -36,12 +39,41 @@ public class HealthCheck {
 		}
 	}
 	
+	@XmlRootElement(name = "health")
+	public static class HealthXML {
+		String message = "up and runnings";
+		@XmlElement
+		public String getMessage() {
+			return message;
+		}
+		@XmlElement
+		public String getStatus() {
+			return status;
+		}
+		String status = "OK";
+		
+		public HealthXML(){}
+		public HealthXML(String message, String status){
+			this.message = message;
+			this.status = status;
+		}
+	}
+	
 	@GET
 	@Produces("application/json")
 	@Path("/check")
 	@Formatted
 	public Health getHealthCheck() {
 		return new Health("hello michael", "OK");
+	}
+	
+	@GET
+	@Produces("application/xml")
+	@Path("/checkxml")
+	public Response getHealthCheckXML() {
+		HealthXML hx = new HealthXML("hello michael", "OK");
+		GenericEntity<HealthXML> entity = new GenericEntity<HealthXML>(hx){};      
+	    return Response.status(200).entity(entity).type(MediaType.APPLICATION_XML).build();
 	}
 	
 	@GET
